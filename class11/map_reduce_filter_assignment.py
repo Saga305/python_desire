58463995454c9ca1c9b0d393360c8ad40e4c42f0
SagaTheGreat <ajay.patel305@gmail.com>
2024-06-08 00:16:02 +0530

    Modified

--- class11/map_reduce_filter_assignment.py
+++ class11/map_reduce_filter_assignment.py
@@ -3,13 +3,14 @@ numbers = [1, 2, 3, 4, 5]
 
 cubed_numbers = list(map(lambda x: x ** 3, numbers))
 
-even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+even_of_cubed_numbers = list(filter(lambda x: x % 2 == 0, cubed_numbers))
 
-mul_of_numbers = reduce(lambda x, y: x * y, numbers, 0)
+mul_of_even_cubed_numbers = reduce(lambda x, y: x * y, even_of_cubed_numbers, 1)
 
+print("numbers:",numbers)
 print("cubed_numbers:",cubed_numbers)
-print("even_numbers:",even_numbers)
-print("mul_of_numbers:",mul_of_numbers)
+print("even_of_cubed_numbers:",even_of_cubed_numbers)
+print("mul_of_even_cubed_numbers:",mul_of_even_cubed_numbers)
 
 
 
